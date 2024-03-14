392065bf3f7e83d3d987fc63d1560909875ce571
Cristian Tovar <cctovar@grupovestel.com>
2024-03-14 09:16:57 +0100

    Fin de la sección 5

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ];
+
+  @Output()
+  onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    // TODO: Emitir el ID del personaje
+
+    if ( !id ) return;
+
+    this.onDelete.emit(id);
+  }
+
+}
